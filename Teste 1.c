2772f46ab8b33662fe4b17a639cad2d757e37816
caroll-BRA <discovery_49@outlook.com>
2026-02-12 23:13:43 +0000

    Desafio teste 1

--- Teste 1.c
+++ Teste 1.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
-int(){
-    printf("Olá mundo!");
+// Definição de estrutura
+
+typedef struct{
+     char nome[30];
+     char cor[10];
+     int tropas;
+} Territorio;
+
+
+// Definição de constantes
+
+#define MAX_Territorio 40
+
+// Declaração de territórios 
+
+int main () {
+Territorio territorio1 = {"Coreia do Norte","Lilás",2};
+Territorio territorio2 = {"Coreia do Sul","Rosa Pink", 4};
+Territorio territorio3 = {"China","Vermelha",5};
+Territorio territorio4 = {"Taiwan","Dourado",8};
+Territorio territorio5 = {"Japão","Azul",3};
+return 0;
 }
+
+printf("Deseja visualizar territórios?");
+printf("Digite opção SIM ou NÃO");
