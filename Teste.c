12eb6e1175cb451df0eb2af141e7e8c14c132b62
caroll-BRA <discovery_49@outlook.com>
2026-02-17 02:38:35 +0000

    Teste 2

--- Teste.c
+++ Teste.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-printf ("Olá mundo");
+// Declaração de Territórios
+1. Nome: Coreia do Norte, Cor: Lilás, Tropas: 2
+2. Nome: Coreia do Sul, Cor: Rosa Pink, Tropas: 4
+3. Nome: China, Cor: Vermelha, Tropas: 5
+4. Nome:  Taiwan, Cor: Dourado, Tropas: 8
+5. Nome: Japão, Cor: Azul, Tropas: 3
+
+// Definição de estrutura
+int main(){
+typedef struct{
+     char nome[30];
+     char cor[10];
+     int tropas;
+} Territorio;
+
+// Definição de constantes
+
+#define MAX_Territorio 40
+       
+// Declaração de territórios 
+
+while (MAX_Territorio < 4){
+     printf("Digite, nessa squência, território, cor e tropa:");
+     scanf("Nome: %s Cor: %s Tropas: %d", Territorio.nome, Territorio.cor, Territorio.tropas);
+     return = 0;
+}
+
 }
