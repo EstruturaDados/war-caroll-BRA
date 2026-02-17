f802cdf77e707cf9fde1fe84f52df6d1e96f17e8
caroll-BRA <discovery_49@outlook.com>
2026-02-17 02:41:11 +0000

    Teste 3

--- Teste.c
+++ Teste.c
@@ -2,11 +2,11 @@
 #include <string.h>
 
 // Declaração de Territórios
-1. Nome: Coreia do Norte, Cor: Lilás, Tropas: 2
-2. Nome: Coreia do Sul, Cor: Rosa Pink, Tropas: 4
-3. Nome: China, Cor: Vermelha, Tropas: 5
-4. Nome:  Taiwan, Cor: Dourado, Tropas: 8
-5. Nome: Japão, Cor: Azul, Tropas: 3
+// 1. Nome: Coreia do Norte, Cor: Lilás, Tropas: 2
+// 2. Nome: Coreia do Sul, Cor: Rosa Pink, Tropas: 4
+// 3. Nome: China, Cor: Vermelha, Tropas: 5
+// 4. Nome:  Taiwan, Cor: Dourado, Tropas: 8
+// 5. Nome: Japão, Cor: Azul, Tropas: 3
 
 // Definição de estrutura
 int main(){
@@ -24,7 +24,7 @@ typedef struct{
 
 while (MAX_Territorio < 4){
      printf("Digite, nessa squência, território, cor e tropa:");
-     scanf("Nome: %s Cor: %s Tropas: %d", Territorio.nome, Territorio.cor, Territorio.tropas);
+     fgets("Nome: %s Cor: %s Tropas: %d", Territorio.nome, Territorio.cor, Territorio.tropas);
      return = 0;
 }
 
